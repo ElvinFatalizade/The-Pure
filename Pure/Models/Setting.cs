9ad000f0952bc6d61233aac94d2c2379f90e0182
Elvin Fatalizade <elvinff@code.edu.az>
2019-10-13 00:44:58 +0400

    Home, Profile, Store, Contact views complate

---
+++ Pure/Models/Setting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pure.Models
+{
+    public class Setting
+    {
+        public int Id { get; set; }
+
+        public string Location  { get; set; }
+
+        public string Email { get; set; }
+
+        public string  Number { get; set; }
+    }
+}
