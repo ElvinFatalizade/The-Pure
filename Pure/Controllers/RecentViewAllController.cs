ed54171d849c2fedc823b303734d477c7dfeca7a
Elvin Fatalizade <elvinff@code.edu.az>
2019-10-25 19:25:45 +0400

    All Views Complate

---
+++ Pure/Controllers/RecentViewAllController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pure.Controllers
+{
+    public class RecentViewAllController : Controller
+    {
+        // GET: RecentViewAll
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
